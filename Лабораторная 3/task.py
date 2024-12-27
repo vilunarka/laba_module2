2e78afc7886c279ce11ffbe16e6791642f819759
vilunarka <bunina.vika876@gmail.com>
2024-12-27 18:33:19 +0300

    Add files via upload

---
+++ Лабораторная 3/task.py
@@ -0,0 +1,54 @@
+class Book:
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, number_of_pages):
+        if not isinstance(number_of_pages, int):
+            raise TypeError("Количество страниц должно быть целым числом")
+        if number_of_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = number_of_pages
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, book_duration):
+        if not isinstance(book_duration, (int, float)):
+            raise TypeError("Продолжительность должна быть типа int или float")
+        if book_duration <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = book_duration
